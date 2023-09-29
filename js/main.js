4282ced99af003410d25a6ba0283447631836a4c
Tilo <tilogaray_fg@hotmail.com>
2023-09-28 22:03:34 -0300

    Agrego array y objetos para mi segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,38 +1,111 @@
-function realizarOperacion() {
-    const numero1 = parseFloat(prompt("Ingrese el primer número"));
-    const numero2 = parseFloat(prompt("Ingrese el segundo número"));
-    
-    if (isNaN(numero1) || isNaN(numero2)) {
-        alert("Por favor, ingrese números válidos.");
-        return;
-    }
+// Declaración de variables necesarias
+let resultado;
 
-    const operacion = prompt("Seleccione una operación: suma, resta, multiplicación o división").toLowerCase();
-    
-    let resultado;
+// Declaración de un objeto para manejar operaciones
+const operaciones = {
+    suma: function (numero1, numero2) {
+        return numero1 + numero2;
+    },
+    resta: function (numero1, numero2) {
+        return numero1 - numero2;
+    },
+    multiplicacion: function (numero1, numero2) {
+        return numero1 * numero2;
+    },
+    division: function (numero1, numero2) {
+        if (numero2 !== 0) {
+            return numero1 / numero2;
+        } else {
+            return "Error: División por cero";
+        }
+    },
+    concatenacion: function (texto1, texto2) {
+        return texto1 + texto2;
+    },
+    porcentaje: function (numero, porcentaje) {
+        return (numero * porcentaje) / 100;
+    }
+};
+
+// Declaración de un array para almacenar resultados
+const resultados = [];
+
+// Función para realizar operaciones
+function realizarOperacion() {
+    const operacion = prompt("Ingrese una operación: suma, resta, multiplicación, división, concatenación o porcentaje").toLowerCase();
 
     switch (operacion) {
         case "suma":
-            resultado = numero1 + numero2;
-            break;
         case "resta":
-            resultado = numero1 - numero2;
-            break;
         case "multiplicacion":
-            resultado = numero1 * numero2;
-            break;
         case "division":
-            if (numero2 !== 0) {
-                resultado = numero1 / numero2;
+        case "concatenacion":
+        case "porcentaje":
+            const input1 = prompt("Ingrese el primer valor");
+            const input2 = prompt("Ingrese el segundo valor");
+
+            const valor1 = parseFloat(input1);
+            const valor2 = parseFloat(input2);
+
+            if (isNaN(valor1) || isNaN(valor2)) {
+                resultado = "Por favor, ingrese valores numéricos válidos.";
             } else {
-                resultado = "Error: División por cero";
+                resultado = operaciones[operacion](valor1, valor2);
+                resultados.push(resultado);
             }
             break;
         default:
             resultado = "Operación no válida";
     }
 
-    alert("El resultado es: " + resultado);
+    mostrarResultado(resultado);
 }
 
-realizarOperacion();
+// Función para mostrar el resultado
+function mostrarResultado(resultado) {
+    alert("El resultado es: " + resultado);
+    mostrarResultadosAnteriores();
+}
+
+// Función para mostrar resultados anteriores
+function mostrarResultadosAnteriores() {
+    if (resultados.length > 0) {
+        alert("Resultados anteriores: " + resultados.join(", "));
+    }
+}
+
+// Función para buscar un resultado específico
+function buscarResultado() {
+    const valorBuscado = parseFloat(prompt("Ingrese el valor que desea buscar"));
+
+    if (isNaN(valorBuscado)) {
+        alert("Por favor, ingrese un valor numérico válido.");
+        return;
+    }
+
+    const indice = resultados.indexOf(valorBuscado);
+
+    if (indice !== -1) {
+        alert(`El valor ${valorBuscado} se encuentra en la posición ${indice} de los resultados.`);
+    } else {
+        alert(`El valor ${valorBuscado} no se encuentra en los resultados.`);
+    }
+}
+
+// Función para filtrar resultados por valor
+function filtrarResultadosPorValor() {
+    const valorFiltro = parseFloat(prompt("Ingrese el valor por el cual desea filtrar los resultados"));
+
+    if (isNaN(valorFiltro)) {
+        alert("Por favor, ingrese un valor numérico válido.");
+        return;
+    }
+
+    const resultadosFiltrados = resultados.filter(resultado => resultado === valorFiltro);
+
+    if (resultadosFiltrados.length > 0) {
+        alert("Resultados filtrados: " + resultadosFiltrados.join(", "));
+    } else {
+        alert(`No se encontraron resultados iguales a ${valorFiltro}.`);
+    }
+}
